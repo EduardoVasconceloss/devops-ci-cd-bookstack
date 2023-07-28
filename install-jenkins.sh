a06a4775304dfe1e72851ca94545985cd0d82ec3
Eduardo Vasconcelos <107057997+EduardoVasconceloss@users.noreply.github.com>
2023-07-28 10:51:18 -0300

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -8,5 +8,6 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt update  -y
-sudo apt install -y fontconfig openjdk-17-jre-headless openjdk-17-jre jenkins
+sudo apt update -y
+sudo apt install -y fontconfig openjdk-17-jre-headless openjdk-17-jre 
+sudo apt install -y jenkins
