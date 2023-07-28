b713b03253d3a8e6a889f53f2f45f56c0ee635b1
eduardo <eduardo.lbvasconcelos@gmail.com>
2023-07-27 23:22:37 -0300

    adicionando o script do jenkins

---
+++ jenkins-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script de instalação do Jenkins no Debian 12
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre-headless openjdk-17-jre jenkins
